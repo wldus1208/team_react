6f30246b6bf41a7b257dc7a26abc7d998bc4aab6
vcomet1 <163480133+vcomet1@users.noreply.github.com>
2024-03-26 09:50:41 +0900

    Add files via upload

--- src/main/java/kr/happyjob/study/service/login/LoginService2.java
+++ src/main/java/kr/happyjob/study/service/login/LoginService2.java
@@ -5,24 +5,25 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kr.happyjob.study.model.login.LoginVO;
 import kr.happyjob.study.repository.login.LoginDAO;
 
 @Service
-public class LoginServiceImpl implements LoginService{
-	
-	private LoginDAO loginDAO;
-	
-	@Autowired
-	public LoginServiceImpl(LoginDAO loginDAO) {
-		this.loginDAO = loginDAO;
-		
-	}
+public class LoginService2 {
 
-	@Override
+	@Autowired
+	private LoginDAO loginDAO;
+
+	// 회원가입
 	public int registerGo(Map<String, Object> paramMap) throws Exception {
-		
+		System.out.println(paramMap);
+		System.out.println(paramMap);
 		return loginDAO.registerGo(paramMap);
 	}
-	
-	
+
+	// 로그인
+	public LoginVO loginGo(Map<String, Object> paramMap) {
+		return loginDAO.loginGo(paramMap);
+	}
+
 }
